bfb651011ce6af8aa40f512cd1cadc620b6b78ef
hoa.phan-van <hoa.phan-van@banvien.com.vn>
2021-08-10 18:37:33 +0700

    fix layout mode

--- src/DocumentView/DocumentView.js
+++ src/DocumentView/DocumentView.js
@@ -135,7 +135,7 @@ export default class DocumentView extends PureComponent {
   setLayoutMode = (layoutMode) => {
     const tag = findNodeHandle(this._viewerRef);
     if (tag != null) {
-      DocumentViewManager.setToolMode(tag, layoutMode);
+      DocumentViewManager.setLayoutMode(tag, layoutMode);
     }
   }
 
